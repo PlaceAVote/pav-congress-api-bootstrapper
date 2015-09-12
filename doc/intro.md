1b17dd8c9a7f705250f357c7d9dd1132a29bb8b4
john ervine <johnervine@hotmail.com>
2015-09-12 14:56:03 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pav-congress-api-bootstrapper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pav_congress_api_bootstrapper/core_test.clj
@@ -0,0 +1,7 @@
+(ns pav-congress-api-bootstrapper.core-test
+  (:require [clojure.test :refer :all]
+            [pav-congress-api-bootstrapper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
