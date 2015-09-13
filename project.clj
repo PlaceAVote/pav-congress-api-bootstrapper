f230fa9cd64b6854ef04a0f51dd1a87cd3b749c2
john ervine <johnervine@hotmail.com>
2015-09-13 20:40:04 +0100

    more updates to docker config

--- project.clj
+++ project.clj
@@ -14,10 +14,7 @@
   :main com.pav.congress.main
   :min-lein-version "2.0.0"
   :javac-options ["-target" "1.8" "-source" "1.8"]
-  :profiles {:prod {
-
-                    }
-             :uberjar {:aot :all
+  :profiles {:uberjar {:aot :all
                        :uberjar-name "pav-congress-api-bootstrapper.jar"}
              :dev {
                    :dependencies [[midje "1.7.0"]]
