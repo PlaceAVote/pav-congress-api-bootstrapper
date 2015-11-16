7e6f0297aefb4481deae7f6b884782a10c7505d8
Sanel Zukan <sanel@placeavote.com>
2015-11-16 05:52:12 +0100

    Cleanup.

--- src/com/pav/congress/jobs/committee.clj
+++ src/com/pav/congress/jobs/committee.clj
@@ -4,11 +4,22 @@
             [com.pav.congress.committee.committee :refer [persist-committees]]
             [clojure.tools.logging :as log]))
 
+(defn parse-s3-yaml-file
+  "Parse yaml file found on S3"
+  [object]
+  ;; TODO: this should go to utils
+  (-> object :content slurp parse-string))
+
 (defn sync-committees
   "Synchronize comittees by reading data from S3."
   [es-connection cred s3-info]
-  (let [current-committees (parse-string (slurp (:content (get-object cred (:legislator-bucket s3-info) (:committees-prefix s3-info)))))
-        committee-members (parse-string (slurp (:content (get-object cred (:legislator-bucket s3-info) (:committee-members s3-info)))))
+  (let [bucket (:legislator-bucket s3-info)
+        current-committees (-> cred
+                               (get-object bucket (:committees-prefix s3-info))
+                               parse-s3-yaml-file)
+        committee-members (-> cred
+                              (get-object bucket (:committee-members s3-info))
+                              parse-s3-yaml-file)
         len (count current-committees)]
     (log/infof "Persisting %d Comittees to Elasticsearch" len)
     (persist-committees es-connection current-committees committee-members)
