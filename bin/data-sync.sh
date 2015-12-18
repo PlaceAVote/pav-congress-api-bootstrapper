717e6e578d5b6d7b818cb620d3c2baa48d9ca28e
johnervine@hotmail.com <johnervine@hotmail.com>
2015-12-18 08:37:59 +0000

    No need to import bills for congress 113.

--- bin/data-sync.sh
+++ bin/data-sync.sh
@@ -3,7 +3,6 @@
 echo "Starting S3 sync at: $(date)"
 
 rsync -av --delete --delete-excluded  --exclude **/text-versions/ --exclude **data.xml --exclude **text-versions.json --stats govtrack.us::govtrackdata/congress-legislators share/
-rsync -av --delete --delete-excluded  --exclude **/text-versions/ --exclude **data.xml --exclude **text-versions.json --stats govtrack.us::govtrackdata/congress/113 share/congress/
 rsync -av --delete --delete-excluded  --exclude **/text-versions/ --exclude **data.xml --exclude **text-versions.json --stats govtrack.us::govtrackdata/congress/114 share/congress/
 rsync -av --delete --delete-excluded --exclude archive/** --exclude archive1/** --exclude **.txt --stats govtrack.us::govtrackdata/photos share/
 
