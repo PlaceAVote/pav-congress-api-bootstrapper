54f453a1dacaec68c4d894e841d7115f611a03cf
johnervine@hotmail.com <johnervine@hotmail.com>
2016-01-23 17:50:21 +0000

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pav-congress-api-bootstrapper "0.1.2-SNAPSHOT"
+(defproject pav-congress-api-bootstrapper "0.1.2"
   :description "PlaceAVote bills parser and indexer"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [environ "1.0.0"]
