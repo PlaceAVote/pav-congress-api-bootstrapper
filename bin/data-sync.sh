387b92c7c1e6f2c86bb17a9e2967663ad56c3858
johnervine@hotmail.com <johnervine@hotmail.com>
2016-01-23 17:42:40 +0000

    All legislator images are in s3 bucket.  No need to sync them anymore.

--- bin/data-sync.sh
+++ bin/data-sync.sh
@@ -4,7 +4,7 @@ echo "Starting S3 sync at: $(date)"
 
 rsync -av --delete --delete-excluded  --exclude **/text-versions/ --exclude **data.xml --exclude **text-versions.json --stats govtrack.us::govtrackdata/congress-legislators share/
 rsync -av --delete --delete-excluded  --exclude **/text-versions/ --exclude **data.xml --exclude **text-versions.json --stats govtrack.us::govtrackdata/congress/114 share/congress/
-rsync -av --delete --delete-excluded --exclude archive/** --exclude archive1/** --exclude **.txt --stats govtrack.us::govtrackdata/photos share/
+#rsync -av --delete --delete-excluded --exclude archive/** --exclude archive1/** --exclude **.txt --stats govtrack.us::govtrackdata/photos share/
 
 aws s3 sync share/ "s3://$S3_BUCKET"
 echo "FINISHED RUN AT $(date). SLEEPING FOR 5 HOURS"
