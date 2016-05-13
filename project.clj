a2f7204a2c96361a4f0cfc1fc0118016b0e8e401
johnervine@hotmail.com <johnervine@hotmail.com>
2016-05-13 18:51:24 +0100

    lein-release plugin: bumped version from 0.1.10 to 0.1.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pav-congress-api-bootstrapper "0.1.10"
+(defproject pav-congress-api-bootstrapper "0.1.11-SNAPSHOT"
   :description "PlaceAVote bills parser and indexer"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [environ "1.0.0"]
